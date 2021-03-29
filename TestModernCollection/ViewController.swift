f95a4c11c62fb77b5fa9941c26985d17def6fe50
ns-yamashita <daito.yamashita@na-s.jp>
2021-03-29 11:57:24 +0900

    storyboardにCollectionViewを表示

--- TestModernCollection/ViewController.swift
+++ TestModernCollection/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+   
+    let numbers = [Int](0..<100)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let collectionViewCell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        let labelNumber = collectionViewCell.contentView.viewWithTag(0) as! UILabel
+        labelNumber.text = String(numbers[indexPath.row])
+        
+        return collectionViewCell
+    }
 }
 
